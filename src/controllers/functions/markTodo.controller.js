2a62c2531a550492c1e792ac06b25fb1439fa5f7
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2022-10-26 13:23:54 +0530

    push check

--- src/controllers/functions/markTodo.controller.js
+++ src/controllers/functions/markTodo.controller.js
@@ -3,8 +3,7 @@ import todo from "../../models/todo.js";
 export const markTodo=async(req,res)=>{
     try {
         const todof=await todo.findOneAndUpdate({
-            _id:req.body.todo_id,
-            userId:req.userId
+            _id:req.body.todo_id
 
         },[
             {
